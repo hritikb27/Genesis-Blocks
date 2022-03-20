edc405c695507e953673d998a8941116a94e868a
rdkgpian <32013812+rdkgpian@users.noreply.github.com>
2022-03-20 21:59:37 +0530

    Add files via upload

--- contracts/SupplyChain.sol
+++ contracts/SupplyChain.sol
@@ -8,6 +8,9 @@ contract SupplyChain is User {
   struct Node{
     address owner;
     uint ownerId;
+    string ownerName;
+    string ownerLocation;
+    string itemName;
     uint itemid;
     uint[] precedents;
     bool isRetail;
@@ -29,28 +32,34 @@ contract SupplyChain is User {
   uint totalNodes=1;        //Keeps track of the index of the current node
   mapping(uint => uint) lastNodes;    //ItemID mapped to the last node (nodeID) of that particular item
   mapping(uint => mapping(uint => uint)) itemIdtoOwnerIdtoNodeId;  //This mappping helps in the case when we add a connection to the node for the second or subsequent times. It will help us access the nodeID which was assigned to this node when it was first created.
-  event orderReceived(uint _orderid);
+ 
+ event orderReceived(uint _orderid);
   event History(uint parent_itemId, uint[] children_itemIds);
   event orderShipped(uint _orderid);
   event StartingNode(uint _node);
   event orderRequested (uint _orderid);
 
+  modifier checkUser(uint _userId) { 
+    require (userIdtoAddress[_userId] == msg.sender, "Wrong user"); 
+    _; 
+  }
+  
 
   //Adds new user to the Dapp
-  function addStartingNode(uint _userId, uint _itemId, string memory _itemName, bool _isRetail, uint _cost) external returns(uint){
+  function addStartingNode(uint _userId, string memory _userName, uint _itemId, string memory _itemName,  string memory _location, bool _isRetail, uint _cost) external checkUser(_userId) returns(uint){
     uint [] memory firstPrecedent;
     uint node = totalNodes;
-    nodes[node] = Node(msg.sender, _userId, _itemId, firstPrecedent, _isRetail, _cost);
+    nodes[node] = Node(msg.sender, _userId, _userName, _location, _itemName, _itemId, firstPrecedent, _isRetail, _cost);
     lastNodes[_itemId]=node;
     itemIdtoOwnerIdtoNodeId[_itemId][_userId]= node;
-    userStock[_userId].push(Stock(_itemId, _itemName,_cost,_isRetail));
+    userStock[_userId].push(Stock(node, _itemId, _itemName,_cost,_isRetail, _location));
     totalNodes+=1;
     emit StartingNode(node);
     return node;
   }
 
 
-  function addTransaction(uint _userId, uint _orderID, uint _itemId, string memory _itemName, bool _isRetail, uint _cost) external payable {
+  function addTransaction(uint _userId, string memory _userName, string memory _location, uint _orderID, uint _itemId, string memory _itemName, bool _isRetail, uint _cost) external checkUser(_userId) payable {
 
       //uint prevNodeId=1;
       uint prevNodeId= orders[_orderID].prevItemNodeId;
@@ -65,10 +74,11 @@ contract SupplyChain is User {
       if(itemIdtoOwnerIdtoNodeId[_itemId][_userId] == 0) {   //First time taking raw material for a particular node (itemid,owner)
         uint[] memory firstPrecedent= new uint[](1);     //way to create dynamic "memory" array in Solidity
         firstPrecedent[0]=prevNodeId;   
-        nodes[totalNodes]= Node(userIdtoAddress[_userId], _userId, _itemId, firstPrecedent, _isRetail, _cost);
+        nodes[totalNodes]= Node(userIdtoAddress[_userId], _userId, _userName, _location, _itemName, _itemId, firstPrecedent, _isRetail, _cost);
        // uint node= totalNodes;
         itemIdtoOwnerIdtoNodeId[_itemId][_userId] = totalNodes;
         lastNodes[_itemId]=totalNodes;
+        updateStockforBuyer(totalNodes, _userId,_itemId, _itemName, _isRetail, _cost, _location);
         totalNodes+=1;
 
       }
@@ -78,10 +88,11 @@ contract SupplyChain is User {
         uint currNodeId= itemIdtoOwnerIdtoNodeId[_itemId][_userId];
         nodes[currNodeId].precedents.push(prevNodeId);
         nodes[currNodeId].cost=_cost;
+        updateStockforBuyer(currNodeId, userId,_itemId, _itemName, _isRetail, _cost, _location);
       }
 
       uint sellerId= nodes[prevNodeId].ownerId;
-      updateStockforBuyer(_userId,_itemId, _itemName, _isRetail, _cost);
+      
       deleteRequestedOrders(_userId, _orderID);
       deleteReceivedOrders(sellerId, _orderID);
 
@@ -90,12 +101,12 @@ contract SupplyChain is User {
   }
 
 
-  function isLastNode(uint _node) public view returns (bool) {
+  function isLastNode(uint _node) internal view returns (bool) {
 
      return lastNodes[nodes[_node].itemid] == _node;
   }
 
-  function orderRequest(uint _buyerId, uint _itemId, string memory _itemName, uint _sellerId) external returns(uint) {
+  function orderRequest(uint _buyerId, uint _itemId, string memory _itemName, uint _sellerId) external checkUser(_buyerId) returns(uint) {
 
    // orders[orderID]= Order(_buyerId, _sellerId, _itemId, 1);
    uint prevNodeId = itemIdtoOwnerIdtoNodeId[_itemId][_sellerId];
@@ -119,7 +130,7 @@ contract SupplyChain is User {
 
 
 
-  function orderShipping(uint _orderID) external {
+  function orderShipping(uint _userId, uint _orderID) external checkUser(_userId) {
     uint sellerId= orders[_orderID].sellerId;
     require(userIdtoAddress[sellerId]== msg.sender);
     orderShippingSatus[_orderID]= true;
@@ -128,64 +139,28 @@ contract SupplyChain is User {
     emit orderShipped(_orderID);
   }
 
-  uint[] queue;
 
-  int first;
-  int last;
-      //uint size=0;
-  function enqueue(uint data) internal {
-    last += 1;
-        //  queue[last] = data;
-    queue.push(data);
-    if(queue.length==1)
-      first=0;
-  }
+  //Returns the details of the precedents of the given node
+  function getPrecedents(uint _node) external view returns (string[] memory, string[] memory, string[] memory){
 
-  function dequeue() internal {
-    require(last >= first);  // non-empty queue
-    //uint data = queue[first];
-    delete queue[uint(first)];
-    first += 1;
-    if(queue.length==0)
-    {
-      first=-1;
-      last=-1;
+    //uint [] memory precedentsArray = new uint[](nodes[_node].precedents.length);
+    uint numberofPrecedents = nodes[_node].precedents.length;
+    string[] memory ownersNames = new string[](numberofPrecedents);
+    string[] memory ownersLocations = new string[](numberofPrecedents);
+    string[] memory itemsNames = new string[](numberofPrecedents);
+
+    for(uint i=0; i< numberofPrecedents; i++) {
+
+      uint nodeId = nodes[_node].precedents[i];
+      string memory ownerName = nodes[_node].ownerName;
+      string memory ownerLocation = nodes[_node].ownerLocation;
+      string memory itemName = nodes[_node].itemName;
+      ownersNames[i]= ownerName;
+      ownersLocations[i] = ownerLocation;
+      itemsNames[i] = itemName;
     }
-  }
 
-
-  function getHistory(uint _itemId) external {
-
-    require( nodes[lastNodes[ _itemId]].isRetail == true);
-
-    //currNode = nodes[lastNodes[_itemId]];
-   // mapping(uint => bool) memory visited;     //NodeID to visted or not flag mapping
-    delete queue;
-    first=-1;
-    last=-1;
-    enqueue(lastNodes[_itemId]);
-    //visited[lastNodes[_itemId]] = true;
-
-    while(last>=0 && first>=0 && last>=first) // Condition to check that the queue is not empty 
-    {
-
-        uint currNodeid= queue[uint(first)];
-        uint[] memory children_itemIds= new uint[](nodes[currNodeid].precedents.length);
-        dequeue();
-
-        for(uint i=0; i<nodes[currNodeid].precedents.length; i++) {
-
-          //if(visited[nodes[currNodeid].precedents[i]] != true) {
-          uint temp_itemId= nodes[nodes[currNodeid].precedents[i]].itemid;
-          enqueue(nodes[currNodeid].precedents[i]);
-          children_itemIds[i]= temp_itemId;
-         // last=last-1;
-        
-        }
-      emit History(nodes[currNodeid].itemid, children_itemIds);
-
-    
-    }
+    return (ownersNames, ownersLocations, itemsNames);
   }
 
 }
