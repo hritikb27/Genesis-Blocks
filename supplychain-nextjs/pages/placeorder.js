630c468641b9742eab505a7656f92b0354928251
unknown <hritikb90@gmail.com>
2022-03-23 20:51:12 +0530

    @hritikb27 Solved the getstock data displaying issue

--- supplychain-nextjs/pages/placeorder.js
+++ supplychain-nextjs/pages/placeorder.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from "react"
 import useSWR from "swr";
 import AppContext from "../components/AppContext"
+import bigNumber from "big-number";
 
 export default function PlaceOrder() {
     const {supplyContract, currentAddress, users} = useContext(AppContext);
@@ -31,7 +32,19 @@ export default function PlaceOrder() {
             }
         //     
             const userdata = await supplyContract.getStock(user.userId)
-            setState(prev=>[...prev, userdata])
+            if(userdata.includes(Object)){
+                console.log('Object Found')
+            }
+
+            const StringObj = userdata.map(data=>{
+                console.log('String DATA',data);
+                return data.map(item=>{
+                    console.log('String ITEM', String(item))
+                    return String(item)
+                });
+            })
+            console.log('StringObj', StringObj)
+            setState(prev=>[...prev, StringObj])
         //         
             
             
@@ -44,17 +57,23 @@ export default function PlaceOrder() {
         <div className="w-full flex mt-6 text-white">
             <div className='flex flex-col justify-center items-center w-full'>
                 <h1>Available Products:</h1>
-                <div className="w-[20%] mt-6 flex flex-col gap-4">
-                    <h1>Hello</h1>
+                <div className="w-[90%] mt-6 flex justify-center flex-wrap flex gap-4">
                     {state.length>=1 && state.map(user=>{
                         return user.map(item=>{
-                            console.log('ITEM',item)
-                            return item.map(item2=>{
-                                console.log('ITEM 2: ', typeof item2)
-                                if(typeof item2===Object){
-                                    return <h1 key={item2}>{item2.itemName}</h1>
-                                }
-                            })
+                            return <div key={item} className='bg-gray-500 w-[15%] h-[250px] rounded-lg text-xl flex flex-col justify-center items-between'>
+                                <table className="text-center flex flex-col gap-4 items-center">
+                                <tr>{item[2]}</tr>
+                                <tr>${item[3]}</tr>
+                                <tr>{item[5]}</tr>
+                                <button className="w-[50%] h-[30px] rounded-md bg-red-500">Order</button>
+                                </table>
+                                <br/>
+                            </div>
+                            // return item.map(item2=>{
+                            //     return <h1 key={item2}>{item2}</h1>
+                                
+                            // })
+                            
                         })
                     })}
                 </div>
